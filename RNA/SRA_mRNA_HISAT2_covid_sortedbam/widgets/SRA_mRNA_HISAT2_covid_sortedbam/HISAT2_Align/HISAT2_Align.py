9e305ffc3bd6e6b35515975f54458c43e9930dcc
BryceNoFu <brycefukuda@gmail.com>
2023-08-30 06:15:34 +0000

    Have covid workflow versions without sorted bams

---
+++ RNA/SRA_mRNA_HISAT2_covid_sortedbam/widgets/SRA_mRNA_HISAT2_covid_sortedbam/HISAT2_Align/HISAT2_Align.py
@@ -0,0 +1,75 @@
+import os
+import glob
+import sys
+import functools
+import jsonpickle
+from collections import OrderedDict
+from Orange.widgets import widget, gui, settings
+import Orange.data
+from Orange.data.io import FileFormat
+from DockerClient import DockerClient
+from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
+from PyQt5 import QtWidgets, QtGui
+
+class OWHISAT2_Align(OWBwBWidget):
+    name = "HISAT2_Align"
+    description = "HISAT2 FASTQ Alignment"
+    priority = 10
+    icon = getIconName(__file__,"ogp.png")
+    want_main_area = False
+    docker_image_name = "biodepot/hisat2"
+    docker_image_tag = "latest"
+    inputs = [("Trigger",str,"handleInputsTrigger"),("mate_1",str,"handleInputsmate_1"),("mate_2",str,"handleInputsmate_2"),("sam_output",str,"handleInputssam_output"),("hisat2_idx",str,"handleInputshisat2_idx")]
+    outputs = [("sam_output",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    hisat2_idx=pset(None)
+    mate_1=pset([])
+    mate_2=pset([])
+    sam_output=pset([])
+    unpaired=pset([])
+    sraids=pset([])
+    nthreads=pset(None)
+    def __init__(self):
+        super().__init__(self.docker_image_name, self.docker_image_tag)
+        with open(getJsonName(__file__,"HISAT2_Align")) as f:
+            self.data=jsonpickle.decode(f.read())
+            f.close()
+        self.initVolumes()
+        self.inputConnections = ConnectionDict(self.inputConnectionsStore)
+        self.drawGUI()
+    def handleInputsTrigger(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("Trigger", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputsmate_1(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("mate_1", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputsmate_2(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("mate_2", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputssam_output(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("sam_output", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputshisat2_idx(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("hisat2_idx", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleOutputs(self):
+        outputValue=None
+        if hasattr(self,"sam_output"):
+            outputValue=getattr(self,"sam_output")
+        self.send("sam_output", outputValue)
---
+++ RNA/SRA_mRNA_HISAT2_covid_sortedbam/widgets/SRA_mRNA_HISAT2_covid_sortedbam/Start/Start.py
@@ -0,0 +1,111 @@
+import os
+import glob
+import sys
+import functools
+import jsonpickle
+from collections import OrderedDict
+from Orange.widgets import widget, gui, settings
+import Orange.data
+from Orange.data.io import FileFormat
+from DockerClient import DockerClient
+from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
+from PyQt5 import QtWidgets, QtGui
+
+class OWStart(OWBwBWidget):
+    name = "Start"
+    description = "Enter workflow parameters and start"
+    priority = 10
+    icon = getIconName(__file__,"start.png")
+    want_main_area = False
+    docker_image_name = "biodepot/hisat2-mrna-start"
+    docker_image_tag = "latest"
+    outputs = [("work_dir",str),("genome_dir",str),("genome_file",str),("sra_ids",str),("mate_1",str),("mate_2",str),("unpaired",str),("sam_output",str),("hisat2_idx",str),("annotation_gtf",str),("gzip",str),("bam_output",str),("sorted_bam_output",str),("metafile",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    work_dir=pset(None)
+    genome_file=pset(None)
+    sra_ids=pset([])
+    genome_dir=pset(None)
+    basename=pset("genome")
+    annotation_gtf=pset(None)
+    mate_1=pset([])
+    mate_2=pset([])
+    unpaired=pset([])
+    sam_output=pset([])
+    hisat2_idx=pset(None)
+    gzip=pset(False)
+    sorted_bam_output=pset([])
+    bam_output=pset([])
+    sraidswt=pset([])
+    metafile=pset([])
+    sraidsmutant=pset([])
+    def __init__(self):
+        super().__init__(self.docker_image_name, self.docker_image_tag)
+        with open(getJsonName(__file__,"Start")) as f:
+            self.data=jsonpickle.decode(f.read())
+            f.close()
+        self.initVolumes()
+        self.inputConnections = ConnectionDict(self.inputConnectionsStore)
+        self.drawGUI()
+    def handleOutputs(self):
+        outputValue=None
+        if hasattr(self,"work_dir"):
+            outputValue=getattr(self,"work_dir")
+        self.send("work_dir", outputValue)
+        outputValue=None
+        if hasattr(self,"genome_dir"):
+            outputValue=getattr(self,"genome_dir")
+        self.send("genome_dir", outputValue)
+        outputValue=None
+        if hasattr(self,"genome_file"):
+            outputValue=getattr(self,"genome_file")
+        self.send("genome_file", outputValue)
+        outputValue=None
+        if hasattr(self,"sra_ids"):
+            outputValue=getattr(self,"sra_ids")
+        self.send("sra_ids", outputValue)
+        outputValue=None
+        if hasattr(self,"mate_1"):
+            outputValue=getattr(self,"mate_1")
+        self.send("mate_1", outputValue)
+        outputValue=None
+        if hasattr(self,"mate_2"):
+            outputValue=getattr(self,"mate_2")
+        self.send("mate_2", outputValue)
+        outputValue=None
+        if hasattr(self,"unpaired"):
+            outputValue=getattr(self,"unpaired")
+        self.send("unpaired", outputValue)
+        outputValue=None
+        if hasattr(self,"sam_output"):
+            outputValue=getattr(self,"sam_output")
+        self.send("sam_output", outputValue)
+        outputValue=None
+        if hasattr(self,"hisat2_idx"):
+            outputValue=getattr(self,"hisat2_idx")
+        self.send("hisat2_idx", outputValue)
+        outputValue=None
+        if hasattr(self,"annotation_gtf"):
+            outputValue=getattr(self,"annotation_gtf")
+        self.send("annotation_gtf", outputValue)
+        outputValue=None
+        if hasattr(self,"gzip"):
+            outputValue=getattr(self,"gzip")
+        self.send("gzip", outputValue)
+        outputValue=None
+        if hasattr(self,"bam_output"):
+            outputValue=getattr(self,"bam_output")
+        self.send("bam_output", outputValue)
+        outputValue=None
+        if hasattr(self,"sorted_bam_output"):
+            outputValue=getattr(self,"sorted_bam_output")
+        self.send("sorted_bam_output", outputValue)
+        outputValue=None
+        if hasattr(self,"metafile"):
+            outputValue=getattr(self,"metafile")
+        self.send("metafile", outputValue)
